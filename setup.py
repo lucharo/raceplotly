2919cb95636e479d23532dee6918552921277dfe
Luis Chaves Rodriguez <47890755+lc5415@users.noreply.github.com>
2020-10-05 12:56:42 +0100

    added dependencies manually

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/lc5415/raceplotly",
-    packages=setuptools.find_packages(),
+    install_requires=['pandas','plotly'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
