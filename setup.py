b1e4726ed346c27d7763f6c436d93f3991861dcc
Luis Chaves <lc5415@ic.ac.uk>
2020-10-05 11:47:21 +0100

    Initial Commit 2

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="raceplotly",
+    version="0.1",
+    author="Luis Chaves",
+    author_email="lc5415@ic.ac.uk",
+    description="Tiny package to make 'race' barplots using plotly",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lc5415/raceplotly",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
