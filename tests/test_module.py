bdf6bd1085958c5b7b71cf724c741fb517772dd5
Luis Chaves <lc5415@ic.ac.uk>
2020-10-05 14:11:48 +0100

    pytest is very different to R testing. I should test each function in my class but instead I am just asserting whether the plots run

---
+++ tests/test_module.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import pandas as pd
+from raceplotly.plots import barplot
+import pytest
+# load data
+data = pd.read_csv('https://raw.githubusercontent.com/lc5415/raceplotly/main/example/FAOSTAT_data.csv')
+
+# instantiate barplot
+bar_race = barplot(data, item_column='Item', value_column='Value', time_column='Year')
+
+def test_plot_horizontal():
+    # plot horizontal version
+    bar_race.plot(title = 'Top 10 Crops from 1961 to 2018',
+                  item_label = 'Top 10 crops',
+                  value_label = 'Production quantity (tonnes)', frame_duration = 800)
+    assert True
+
+def test_plot_vertical():
+    # plot vertically
+    bar_race.plot(title = 'Top 10 Crops from 1961 to 2018',
+                  orientation='vertical',
+                  item_label='Crops')
+
