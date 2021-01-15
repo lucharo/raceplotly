76f92f95d53d1c5c19cb90753fcb027c10264821
patils2 <siddarth3639@gmail.com>
2021-01-15 05:51:33 +0530

    Update FAO_with_color_example.py

--- example/FAO_with_color_example.py
+++ example/FAO_with_color_example.py
@@ -7,15 +7,20 @@ data = pd.read_csv('https://raw.githubusercontent.com/lc5415/raceplotly/main/exa
 # To add specific color to the categories, a new coloumn with rgb values for each category has to be created.
 # Assigning colors to the categories.
 colors = {'Sugar cane': 'rgba(0, 76, 109, 1)',
-          'Potatoes': 'rgba(40, 95, 127, 1)',
-          'Wheat': 'rgba(66, 114, 146, 1)',
-          'Rice, paddy':'rgb(208, 210, 211)',
-          'Maize':'rgb(208, 210, 211)',
+          'Potatoes': 'rgb(208, 210, 211)',
+          'Wheat': 'rgb(208, 210, 211)',
+          'Rice, paddy':'rgba(66, 114, 146, 1)',
+          'Maize':'rgba(40, 95, 127, 1)',
           'Sugar beet':'rgb(208, 210, 211)',
           'Rice, paddy (rice milled equivalent)':'rgb(208, 210, 211)',
           'Sweet potatoes':'rgb(208, 210, 211)',
           'Barley':'rgb(208, 210, 211)',
-          'Cassava':'rgb(208, 210, 211)'}
+          'Cassava':'rgb(208, 210, 211)',
+          'Soybeans':'rgb(208, 210, 211)',
+          'Oil palm fruit':'rgb(208, 210, 211)',
+          'Vegetables, fresh nes':'rgb(208, 210, 211)'}
+
+# Default color for cateogry is black when color is not specifed explicitly
 
 # Mapping the items with the color for the whole dataset.
 data['color'] = data['Item'].map(colors)
